ddfa4ab532e3300d347a8c958d2260f5ecc530c8
NolionsChen <nolionschen@jetfuel.com.tw>
2020-09-04 17:43:28 +0800

    [feature] http delete method Request

--- app/src/main/java/tw/nolions/networkoperation/Repository.kt
+++ app/src/main/java/tw/nolions/networkoperation/Repository.kt
@@ -1,5 +1,7 @@
 package tw.nolions.networkoperation
 
+import retrofit2.Response
+
 object Repository {
 
     private var apiService: Service? = null
@@ -11,6 +13,21 @@ object Repository {
     suspend fun get(foo: String? = null): Resp<GetResp> {
         val result = apiService?.get(foo)
 
+        return Resp(
+            isSuccess = result?.isSuccessful,
+            code = result?.code(),
+            message = result?.message(),
+            data = result?.body() as GetResp
+        )
+    }
+
+    suspend fun delete(foo: String? = null): Resp<GetResp> {
+        val result = apiService?.delete(foo)
+
+        return response(result)
+    }
+
+    private fun <T>response(result: Response<T>?):Resp<GetResp> {
         return Resp(
             isSuccess = result?.isSuccessful,
             code = result?.code(),
--- app/src/main/java/tw/nolions/networkoperation/Service.kt
+++ app/src/main/java/tw/nolions/networkoperation/Service.kt
@@ -1,10 +1,14 @@
 package tw.nolions.networkoperation
 
 import retrofit2.Response
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface Service {
     @GET("/get")
     suspend fun get(@Query("foo") foo: String? = null): Response<GetResp>
+
+    @DELETE("/delete")
+    suspend fun delete(@Query("foo") foo: String? = null): Response<GetResp>
 }
