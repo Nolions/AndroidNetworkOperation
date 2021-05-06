1c857691ffc5b33943b63b6782ff96ce3b3cacc4
NolionsChen <nolionschen@jetfuel.com.tw>
2021-05-06 14:36:49 +0800

    [revise] okhttp使用註解

--- app/src/main/java/tw/nolions/networkoperation/Provider.kt
+++ app/src/main/java/tw/nolions/networkoperation/Provider.kt
@@ -1,22 +1,30 @@
 package tw.nolions.networkoperation
 
+import android.content.Context
+import okhttp3.Cache
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class Provider(api: String) {
+class Provider(private val context: Context, api: String) {
     private val retrofit = Retrofit.Builder()
         .baseUrl(api)
         .client(createHttpClient())
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
+    var cacheSize = 10 * 1024 * 1024 // 10 MB
+
+    val cache: Cache
+        get() = Cache(context.cacheDir, cacheSize.toLong())
+
+
     private fun createHttpClient(): OkHttpClient {
         val httpClient = OkHttpClient.Builder()
-            .followRedirects(false)
-            .followSslRedirects(false)
-            .retryOnConnectionFailure(false)
-            .cache(null)
+            .followRedirects(false)  //是否允許重新定向
+            .followSslRedirects(false) //是否支援 http -> https or https -> http的重新定向
+            .retryOnConnectionFailure(false) // 是否允許錯誤重新嘗試連線
+            .cache(cache) // Cache 設置
         return httpClient.build()
     }
 
